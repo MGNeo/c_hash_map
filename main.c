5571517ba266a3436b82fe51d6a3c9fbc41b0346
Глухманюк Максим Александрович <MGNeo@yandex.ru>
2018-04-10 14:25:25 +0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+п»ї#include <stdio.h>
 #include <stdlib.h>
 #include "c_hash_map.h"
 
-// Функция вычисления хэша для строки.
+// Р¤СѓРЅРєС†РёСЏ РІС‹С‡РёСЃР»РµРЅРёСЏ С…СЌС€Р° РґР»СЏ СЃС‚СЂРѕРєРё.
 size_t hash_func_s(const void *const _key)
 {
     if (_key == NULL) return 0;
@@ -19,7 +19,7 @@ size_t hash_func_s(const void *const _key)
     return hash;
 }
 
-// Функция детального сравнения ключей-строк.
+// Р¤СѓРЅРєС†РёСЏ РґРµС‚Р°Р»СЊРЅРѕРіРѕ СЃСЂР°РІРЅРµРЅРёСЏ РєР»СЋС‡РµР№-СЃС‚СЂРѕРє.
 size_t comp_func_s(const void *const _a,
                    const void *const _b)
 {
@@ -36,7 +36,7 @@ size_t comp_func_s(const void *const _a,
     return 0;
 }
 
-// Функция печати строкового ключа элемента хэш-отображения.
+// Р¤СѓРЅРєС†РёСЏ РїРµС‡Р°С‚Рё СЃС‚СЂРѕРєРѕРІРѕРіРѕ РєР»СЋС‡Р° СЌР»РµРјРµРЅС‚Р° С…СЌС€-РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ.
 void print_key_func_s(const void *const _key)
 {
     const char *const p_string = *((char**)_key);
@@ -45,7 +45,7 @@ void print_key_func_s(const void *const _key)
     return;
 }
 
-// Функция печати float данных элемента хэш-отображения.
+// Р¤СѓРЅРєС†РёСЏ РїРµС‡Р°С‚Рё float РґР°РЅРЅС‹С… СЌР»РµРјРµРЅС‚Р° С…СЌС€-РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ.
 void print_data_func_f(const void *const _data)
 {
     const float value = *((float*)_data);
@@ -56,7 +56,7 @@ void print_data_func_f(const void *const _data)
 
 int main()
 {
-    // Создание пустого хэш-отображения (ключ - указатель на строку, данные - float).
+    // РЎРѕР·РґР°РЅРёРµ РїСѓСЃС‚РѕРіРѕ С…СЌС€-РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ (РєР»СЋС‡ - СѓРєР°Р·Р°С‚РµР»СЊ РЅР° СЃС‚СЂРѕРєСѓ, РґР°РЅРЅС‹Рµ - float).
     c_hash_map *hash_map = c_hash_map_create(hash_func_s,
                                              comp_func_s,
                                              sizeof(char*),
@@ -64,7 +64,7 @@ int main()
                                              1000,
                                              1.0f);
 
-    // Вставка элементов в хэш множество.
+    // Р’СЃС‚Р°РІРєР° СЌР»РµРјРµРЅС‚РѕРІ РІ С…СЌС€ РјРЅРѕР¶РµСЃС‚РІРѕ.
     const char *const one_key = "one";
     const char *const two_key = "two";
     const char *const three_key = "three";
@@ -79,22 +79,22 @@ int main()
     value = 3.0f;
     c_hash_map_insert(hash_map, &three_key, &value);
 
-    // Печать содержимого хэш-отображения.
+    // РџРµС‡Р°С‚СЊ СЃРѕРґРµСЂР¶РёРјРѕРіРѕ С…СЌС€-РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ.
     c_hash_map_for_each(hash_map, print_key_func_s, print_data_func_f);
 
     printf("\n");
 
-    // Ужимаем хэш-отображение.
+    // РЈР¶РёРјР°РµРј С…СЌС€-РѕС‚РѕР±СЂР°Р¶РµРЅРёРµ.
     c_hash_map_resize(hash_map, 10);
 
-    // Заменяем данные по ключу two_key.
+    // Р—Р°РјРµРЅСЏРµРј РґР°РЅРЅС‹Рµ РїРѕ РєР»СЋС‡Сѓ two_key.
     value = 3.1415f;
     c_hash_map_insert(hash_map, &two_key, &value);
 
-    // Печать содержимого хэш-отображения.
+    // РџРµС‡Р°С‚СЊ СЃРѕРґРµСЂР¶РёРјРѕРіРѕ С…СЌС€-РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ.
     c_hash_map_for_each(hash_map, print_key_func_s, print_data_func_f);
 
-    // Удаление хэш-отображения.
+    // РЈРґР°Р»РµРЅРёРµ С…СЌС€-РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ.
     c_hash_map_delete(hash_map, NULL, NULL);
 
     getchar();
