6f7e7e2dae572b365a52f249f25ab2c3b7ca411b
Глухманюк Максим Александрович <MGNeo@yandex.ru>
2018-04-13 23:24:09 +0400

    Add files via upload

--- c_hash_map.c
+++ c_hash_map.c
@@ -4,7 +4,7 @@
     ОС: Windows 10/x64
     IDE: Code::Blocks 17.12
     Компилятор: default Code::Blocks 17.12 MinGW
-    
+
     Разработчик: Глухманюк Максим
     Эл. почта: mgneo@yandex.ru
     Место: Российская Федерация, Самарская область, Сызрань
@@ -82,42 +82,9 @@ ptrdiff_t c_hash_map_delete(c_hash_map *const _hash_map,
                             void (*const _del_key_func)(void *const _key),
                             void (*const _del_data_func)(void *const _data))
 {
-    if (_hash_map == NULL) return -1;
-
-    if (_hash_map->nodes_count > 0)
+    if (c_hash_map_clear(_hash_map, _del_key_func, _del_data_func) < 0)
     {
-        size_t count = _hash_map->nodes_count;
-        for (size_t s = 0; (s < _hash_map->nodes_count)&&(count > 0); ++s)
-        {
-            if (((void**)_hash_map->slots)[s] != NULL)
-            {
-                void *select_node = ((void**)_hash_map->slots)[s],
-                     *delete_node;
-
-                while (select_node != NULL)
-                {
-                    delete_node = select_node;
-                    select_node = *((void**)select_node);
-
-                    // Если нужно, вызываем для удаления ключа специальную функцию.
-                    if (_del_key_func != NULL)
-                    {
-                        _del_key_func( (uint8_t*)delete_node + sizeof(void*) + sizeof(size_t) );
-                    }
-
-                    // Если нужно, вызываем для удаления данных специальную функцию.
-                    if (_del_data_func != NULL)
-                    {
-                        _del_data_func( (uint8_t*)delete_node + sizeof(void*) + sizeof(size_t)
-                                        + _hash_map->key_size );
-                    }
-
-                    free(delete_node);
-
-                    --count;
-                }
-            }
-        }
+        return -1;
     }
     free(_hash_map->slots);
 
@@ -511,7 +478,7 @@ ptrdiff_t c_hash_map_for_each(const c_hash_map *const _hash_map,
 }
 
 // Очищает хэш-отображение ото всех элементов, сохраняя количество слотов.
-// В случае успеха возвращает > 0.
+// В случае успешной очистки возвращает > 0.
 // Если в хэш-отображении не было элементов, возвращает 0.
 // В случае ошибки возвращает < 0.
 ptrdiff_t c_hash_map_clear(c_hash_map *const _hash_map,
